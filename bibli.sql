a158be323ca38441ff903635a624a1993079c7f2
YasYes <164775715+YasYes@users.noreply.github.com>
2024-05-13 13:57:33 +0200

    Add files via upload

--- bibli.sql
+++ bibli.sql
@@ -1,20 +1,29 @@
 CREATE TABLE User(mail VARCHAR(50) PRIMARY KEY,
 					name VARCHAR(50),
 					first_name VARCHAR(50),
+					age INT,
 					adress VARCHAR(100),
 					number INT,
 					number_borrow INT);
+					
 CREATE TABLE Book(ISBN INT PRIMARY KEY,
 					title VARCHAR(50),
 					kind VARCHAR(50),
 					author VARCHAR(50),
-					time_borrow INT);
-CREATE TABLE Loan(idLoan INT PRIMARY KEY,
+					picture VARCHAR(100),
+					stock INT,
+					time_borrow INT DEFAULT 30);
+					
+CREATE TABLE Loan(idLoan INT AUTO_INCREMENT PRIMARY KEY,
 					idBook INT,
 					idUser VARCHAR(50),
 					duration INT,
-					date DATE,
-					late INT,
+					start_date DATE,
+					end_date DATE,
+					late INT DEFAULT 0,
 					FOREIGN KEY (idBook) REFERENCES Book(ISBN),
 					FOREIGN KEY (idUser) REFERENCES User(mail));
+					
+CREATE TABLE Library(login VARCHAR(50) PRIMARY KEY,
+						password VARCHAR(50));
 					
--- bibli_data.sql
+++ bibli_data.sql
@@ -1,3 +1,8 @@
-INSERT INTO User values('test@gmail.com','name1','firstname1','adress1',065894232515,2);
-INSERT INTO Book values(1234,'title1','kind1','author1',4);
-INSERT INTO Loan values(1,1234,'test@gmail.com',5,'2024-05-12',0);
+INSERT INTO User values('albertroger@gmail.com','Albert','Roger',50,'156 rue des merles chanteurs,Paris',0656667686,2);
+INSERT INTO Book (ISBN, title, kind, author, picture, stock) values (1234,'title1','kind1','author1','picture1.jpg',4);
+INSERT INTO Loan (idBook,idUser,duration,start_date,end_date) values (1234,'albertroger@gmail.com',34,'2024-05-12','2024-06-12');
+INSERT INTO Library values('login1','mdp1');
+
+UPDATE Loan
+SET late = 1
+WHERE duration > 30;
