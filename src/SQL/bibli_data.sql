8b4879a52ad14969752470ef62286d5a0e0dab0e
Yasmina <bouzianeya@cy-tech.fr>
2024-05-23 19:41:30 +0200

    nvl données

--- src/SQL/bibli_data.sql
+++ src/SQL/bibli_data.sql
@@ -21,7 +21,26 @@ INSERT INTO user (mail, name, first_name, birth_date, address, phone_number, num
                                                                                                 ('nezuko@demon.com', 'Nezuko', 'Kamado', '2016-02-15', 'Mount Kumotori', '0679012345', 0),
                                                                                                 ('eren@attack.com', 'Eren', 'Yeager', '2009-09-09', 'Shiganshina', '0670123456', 0);
 
-
+INSERT INTO `borrow` (`idBorrow`, `isbn`, `idUser`, `duration`, `start_date`, `end_date`, `status`) VALUES
+                                                                                                        (1, '9782366130164', 'ash@pokemon.com', 25, '2024-04-28', '2024-05-28', 0),
+                                                                                                        (2, '9782723478991', 'goku@dbz.com', 23, '2024-05-14', '2024-06-13', 1),
+                                                                                                        (3, '9782723478991', 'ash@pokemon.com', 0, '2024-05-23', '2024-06-22', 0),
+                                                                                                        (4, '9782723478991', 'mario@nintendo.com', 6, '2024-05-17', '2024-06-16', 0),
+                                                                                                        (5, '9782723484138', 'nezuko@demon.com', 0, '2024-05-23', '2024-06-22', 0),
+                                                                                                        (6, '9782723470353', 'link@hyrule.com', 26, '2024-04-27', '2024-05-27', 0),
+                                                                                                        (8, '9782723470353', 'sanji@onepiece.com', 31, '2024-05-18', '2024-05-17', 1),
+                                                                                                        (9, '9782350303970', 'vegeta@dbz.com', 20, '2024-05-03', '2024-06-02', 0),
+                                                                                                        (10, '9782350303970', 'zoro@onepiece.com', 18, '2024-05-05', '2024-06-04', 0),
+                                                                                                        (12, '9782350303970', 'tanjiro@demon.com', 20, '2024-04-07', '2024-05-07', 1),
+                                                                                                        (13, '9782350303970', 'sanji@onepiece.com', 23, '2024-04-29', '2024-05-29', 0),
+                                                                                                        (14, '9782363120083', 'luffy@onepiece.com', 26, '2024-04-27', '2024-05-27', 0),
+                                                                                                        (15, '9782363120083', 'samus@metroid.com', 9, '2024-05-14', '2024-06-13', 0),
+                                                                                                        (16, '9782363120083', 'ash@pokemon.com', 15, '2024-01-23', '2024-02-22', 1),
+                                                                                                        (17, '9782363120083', 'naruto@konoha.com', 34, '2024-02-23', '2024-03-24', 1),
+                                                                                                        (19, '9782070624546', 'luigi@nintendo.com', 22, '2024-05-01', '2024-05-31', 0),
+                                                                                                        (21, '9782754811811', 'ash@pokemon.com', 19, '2024-05-05', '2024-06-04', 1),
+                                                                                                        (22, '9782754811811', 'eren@attack.com', 17, '2024-05-06', '2024-06-05', 0),
+                                                                                                        (23, '9782754811811', 'zelda@hyrule.com', 28, '2024-04-25', '2024-05-25', 0);
 INSERT INTO library VALUES('login1','mdp1');
 ALTER TABLE library MODIFY password VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_bin;
 ALTER TABLE library MODIFY login VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_bin;
