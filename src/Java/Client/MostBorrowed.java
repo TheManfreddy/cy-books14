59896d186fb05fc9bc4a17f920e7b02bfbdc71d7
MaximePaviet <maxime.pvet@gmail.com>
2024-05-24 10:56:14 +0200

    modif MostBorrowed pour afficher en tableau

--- src/Java/Client/MostBorrowed.java
+++ src/Java/Client/MostBorrowed.java
@@ -2,6 +2,8 @@ package Client;
 
 import Server.Manager.BookManager;
 import Server.Models.Book;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
@@ -11,25 +13,28 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import Server.Data.APIBNF;
 
 import java.util.List;
 
+/**
+ * Classe MostBorrowed représentant l'interface utilisateur pour afficher les livres les plus empruntés
+ */
 public class MostBorrowed extends VBox {
     private Scene scene;
     private static final int ITEMS_PER_PAGE = 10; // Nombre d'éléments par page
 
     /**
-     * @param primaryStage
-     * @param width
-     * @param height
+     * Constructeur de la classe MostBorrowed
+     *
+     * @param primaryStage le stage principal
+     * @param width        la largeur de la scène
+     * @param height       la hauteur de la scène
      */
     public MostBorrowed(Stage primaryStage, double width, double height) {
 
         // Crée et configure la scène
         BorderPane root = new BorderPane();
         scene = new Scene(root, width, height);
-        /*scene.getStylesheets().add(getClass().getResource("Style/style.css").toExternalForm());*/
 
         // Crée un Label pour le titre
         Label titleLabel = new Label("Les livres les plus empruntés durant les 30 derniers jours");
@@ -40,7 +45,7 @@ public class MostBorrowed extends VBox {
         returnButton.getStyleClass().add("button-UsersPage");
 
         // Crée un conteneur pour le titre et le centre
-        HBox titleBox = new HBox(titleLabel, returnButton);
+        HBox titleBox = new HBox(returnButton, titleLabel);
         titleBox.setAlignment(Pos.CENTER);
         titleBox.setStyle("-fx-padding: 20;");  // Ajoute du padding autour du titre
         root.setTop(titleBox);
@@ -51,100 +56,93 @@ public class MostBorrowed extends VBox {
             LibraryPage librarypage = new LibraryPage(primaryStage, width, height);
             primaryStage.setScene(librarypage.getLibraryPageScene());
         });
-            List<Book> listBook = BookManager.mostBorrowedBooks() ;
 
-            if (listBook != null && !listBook.isEmpty()) {
-                // Convertir la liste en ObservableList pour qu'elle soit compatible avec ListView
-                ObservableList<String> items = FXCollections.observableArrayList();
-                for (Book book : listBook) {
-                    items.addAll(book.getISBN(),book.getLanguage(),book.getTitle(),book.getAuthor(), book.getEditor(), book.getRelease_year()); // Convertir chaque liste de détails en une seule chaîne
-                }
+        List<Book> listBook = BookManager.mostBorrowedBooks();
 
-                // Créer un Pagination pour gérer les pages
-                Pagination pagination = new Pagination((int) Math.ceil((double) items.size() / ITEMS_PER_PAGE), 0);
-                pagination.setPageFactory(pageIndex -> createPage(pageIndex, items, listBook));
+        if (listBook != null && !listBook.isEmpty()) {
+            // Convertir la liste en ObservableList pour qu'elle soit compatible avec TableView
+            ObservableList<Book> items = FXCollections.observableArrayList(listBook);
 
-                // Ajouter le Pagination au centre du BorderPane
-                root.setCenter(pagination);
-            } else {
-                // Gérer le cas où la recherche ne retourne aucun résultat
-                root.setCenter(new Label("Aucun résultat trouvé."));
-            }
+            // Créer un Pagination pour gérer les pages
+            Pagination pagination = new Pagination((int) Math.ceil((double) items.size() / ITEMS_PER_PAGE), 0);
+            pagination.setPageFactory(pageIndex -> createPage(pageIndex, items));
 
-
+            // Ajouter le Pagination au centre du BorderPane
+            root.setCenter(pagination);
+        } else {
+            // Gérer le cas où la recherche ne retourne aucun résultat
+            root.setCenter(new Label("Aucun résultat trouvé."));
+        }
     }
 
     /**
-     * @return
+     * Retourne la scène pour les livres les plus empruntés
+     *
+     * @return la scène
      */
     public Scene getMostBorrowedScene() {
         return scene;
     }
 
     /**
-     * @param language
+     * Crée une page de TableView pour la pagination
+     *
+     * @param pageIndex l'index de la page
+     * @param items     la liste des livres à afficher
+     * @return un VBox contenant la TableView
      */
-    // Méthode appelée lorsque la langue est sélectionnée
-    private void onLanguageSelected(String language) {
-        if (language.equals("FR")) {
-            String FR = "Victor Hugo";
-            String queryFR = "bib.author all " + "\"" + FR + "\"";
-            List<List<String>> listBooks = APIBNF.retrieveBookList(queryFR);
-            System.out.println("Liste : " + listBooks);
-            System.out.println("FR : " + queryFR);
-        }
-        if (language.equals("EN")) {
-            // Ajoutez votre logique ici pour la langue EN
-        }
-    }
-
-    /**
-     * @param pageIndex
-     * @param items
-     * @param listBook
-     * @return
-     */
-    private VBox createPage(int pageIndex, ObservableList<String> items, List<Book> listBook) {
+    private VBox createPage(int pageIndex, ObservableList<Book> items) {
         int fromIndex = pageIndex * ITEMS_PER_PAGE;
         int toIndex = Math.min(fromIndex + ITEMS_PER_PAGE, items.size());
-        ObservableList<String> subList = FXCollections.observableArrayList(items.subList(fromIndex, toIndex));
+        ObservableList<Book> subList = FXCollections.observableArrayList(items.subList(fromIndex, toIndex));
 
-        ListView<String> listView = new ListView<>(subList);
-        listView.setPrefHeight(subList.size() * 24 + 2); // 24 est la hauteur approximative d'une cellule
+        TableView<Book> tableView = new TableView<>(subList);
 
-        // Ajouter un écouteur d'événements pour chaque élément de la liste
-        listView.setOnMouseClicked(event -> {
-            int selectedIndex = listView.getSelectionModel().getSelectedIndex();
-            if (selectedIndex >= 0) {
-                int actualIndex = fromIndex + selectedIndex; // Correction ici pour obtenir l'index réel
-                if (actualIndex < listBook.size()) {
-                    Book selectedBook = listBook.get(actualIndex);
+        TableColumn<Book, String> isbnColumn = new TableColumn<>("ISBN");
+        isbnColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getISBN()));
+
+        TableColumn<Book, String> titleColumn = new TableColumn<>("Titre");
+        titleColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getTitle()));
+
+        TableColumn<Book, String> languageColumn = new TableColumn<>("Langue");
+        languageColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getLanguage()));
+
+        TableColumn<Book, String> authorColumn = new TableColumn<>("Auteur");
+        authorColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getAuthor()));
+
+        TableColumn<Book, String> editorColumn = new TableColumn<>("Éditeur");
+        editorColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getEditor()));
+
+        TableColumn<Book, String> yearColumn = new TableColumn<>("Année");
+        yearColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getRelease_year()));
+
+        TableColumn<Book, Integer> borrowCountColumn = new TableColumn<>("Nombre de fois emprunté");
+        borrowCountColumn.setCellValueFactory(cellData -> new SimpleObjectProperty(cellData.getValue().getBorrowCount()));
+
+        tableView.getColumns().addAll(isbnColumn, titleColumn, languageColumn, authorColumn, editorColumn, yearColumn, borrowCountColumn);
+
+        // Ajouter un écouteur d'événements pour chaque ligne de la table
+        tableView.setRowFactory(tv -> {
+            TableRow<Book> row = new TableRow<>();
+            row.setOnMouseClicked(event -> {
+                if (!row.isEmpty()) {
+                    Book selectedBook = row.getItem();
                     String isbn = selectedBook.getISBN(); // Récupérer l'ISBN
                     System.out.println("Selected ISBN: " + isbn); // Log pour déboguer
                     try {
-                        DisplayMostBorrowed mostborrowed = new  DisplayMostBorrowed((Stage) listView.getScene().getWindow(), scene.getWidth(), scene.getHeight(), isbn);
-                        ((Stage) listView.getScene().getWindow()).setScene(mostborrowed.getDisplayMostBorrowedScene());
+                        DisplayMostBorrowed mostBorrowed = new DisplayMostBorrowed((Stage) tableView.getScene().getWindow(), scene.getWidth(), scene.getHeight(), isbn);
+                        ((Stage) tableView.getScene().getWindow()).setScene(mostBorrowed.getDisplayMostBorrowedScene());
                     } catch (Exception e) {
                         System.err.println("Failed to display book: " + e.getMessage());
                         e.printStackTrace();
                     }
-                } else {
-                    System.err.println("Index out of bounds: " + actualIndex + " for list size: " + listBook.size());
                 }
-            } else {
-                System.err.println("Invalid selection index: " + selectedIndex);
-            }
+            });
+            return row;
         });
 
-
-
-        // Créer une légende
-        HBox legend = new HBox(10, new Label("ISBN"), new Label("Titre"),new Label("Langue") ,new Label("Auteur"), new Label("Éditeur"), new Label("Année"), new Label("Nombre de fois emprunté"));
-        legend.setStyle("-fx-padding: 5; -fx-font-weight: bold;");
-
-        VBox vbox = new VBox(legend, listView);
+        VBox vbox = new VBox(tableView);
         vbox.setAlignment(Pos.TOP_CENTER);
         return vbox;
     }
-
 }
